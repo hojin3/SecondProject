213520cb071856dbd997488543d379a50d5a7256
hojin3 <chuhojin3@gmail.com>
2017-01-24 14:47:04 +0900

    GitHub for git pull

--- Hello.java
+++ Hello.java
@@ -8,6 +8,7 @@ public class Hello{
 		System.out.println("What is Git?");
 		System.out.println("That's DVCS");
 		System.out.println("2002 BitKeeper DVCS");
+		System.out.println("That's version Control");
 
 	}
 }  
